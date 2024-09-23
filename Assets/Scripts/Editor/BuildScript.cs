4f65e7e00b32765cbbc5e5e47ee88235443e80ad
Prathm0025 <prathamesh@underpinservices.com>
2024-09-23 10:02:07 +0530

    Create BuildScript.cs

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,71 @@
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScript
+{
+    public static void PerformBuild()
+    {
+        string[] scenes = { "Assets/Scenes/MainScene.unity" };  // Specify your scenes
+        string buildPath = "Build/WebGL";
+        
+        BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.WebGL, BuildOptions.None);
+        Debug.Log("WebGL Build Completed");
+    }
+}
+
+
+
+using UnityEditor;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Linq;
+using System.IO.Compression;
+
+public class BuildScript
+{
+    public static void BuildWebGL()
+    {
+        string path = "Builds/WebGL";
+        CreateDirectory(path);
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = GetEnabledScenes(),
+            locationPathName = path,
+            target = BuildTarget.WebGL,
+            options = BuildOptions.None
+        };
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        ZipBuild(path);
+    }
+
+    private static string[] GetEnabledScenes()
+    {
+        return EditorBuildSettings.scenes
+            .Where(scene => scene.enabled)
+            .Select(scene => scene.path)
+            .ToArray();
+    }
+
+    private static void CreateDirectory(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    // Method to zip the build directory
+    private static void ZipBuild(string buildPath)
+    {
+        string zipPath = buildPath + ".zip";
+        if (File.Exists(zipPath))
+        {
+            File.Delete(zipPath);
+        }
+        ZipFile.CreateFromDirectory(buildPath, zipPath);
+        Console.WriteLine($"Zipped build to {zipPath}");
+    }
+}
