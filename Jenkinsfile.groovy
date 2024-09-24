0916748a6ca62412c2fa23e59ae1d587c5d18d7a
Prathm0025 <prathamesh@underpinservices.com>
2024-09-24 10:14:43 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,17 @@
 def PROJECT_NAME = "Slot-Vikings-dev"
 def UNITY_VERSION = "2022.3.47f1"
-def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION}\\Editor"
+def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION}\\Editor\\Unity.exe"
 def REPO_URL = "https://github.com/Prathm0025/Slot-Vikings-dev.git"
 def LOG_FILE_PATH = "build.log" // Define the log file path relative to the workspace
 
 pipeline {
-    agent any // Use any available agent
+    agent {
+        label 'Windows-Agent' // Specify the agent by label
+        customWorkspace 'C:\\Jenkins\\Unity_Projects\\Slot-Vikings-dev' // Use the custom workspace
+    }
     
     environment {
-        PROJECT_PATH = "C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Viking-build\\" // Define project path based on Jenkins workspace
+        PROJECT_PATH = "C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Viking-build" // Define project path based on Jenkins workspace
         Token = credentials('GITHUB_TOKEN') // Use GitHub credentials
     }
 
@@ -27,10 +30,10 @@ pipeline {
         stage('Build WebGL') {
             steps {
                 script {
-                    // Ensure the Unity path is correct and does not use workspace path
+                    // Ensure the Unity path is correct
                     withEnv(["UNITY_PATH=${UNITY_INSTALLATION}"]) {
                         bat '''
-                        "%UNITY_PATH%\\Unity.exe" -quit -batchmode -projectPath "%PROJECT_PATH%" -executeMethod BuildScript.BuildWebGL -logFile -
+                        "%UNITY_PATH%" -quit -batchmode -projectPath "%PROJECT_PATH%" -executeMethod BuildScript.BuildWebGL -logFile "%WORKSPACE%\\%LOG_FILE_PATH%"
                         '''
                     }
                 }
@@ -44,12 +47,10 @@ pipeline {
                         // Ensure you are in the build directory
                         bat '''
                         git init
-                        git config user.email "you@example.com"
-                        git config user.name "Your Name"
+                        git config user.email "moreprathamesh849@gmail.com"
+                        git config user.name "Prathm0025"
                         git add .
                         git commit -m "Add WebGL build"
-                        git add "%WORKSPACE%\\%LOG_FILE_PATH%" // Add the log file to the commit
-                        git commit -m "Add build log"
                         git remote add origin ${REPO_URL}
                         git push origin dev-build --force // Push to the correct branch
                         '''
